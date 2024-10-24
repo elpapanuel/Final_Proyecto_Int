27fb2cc4838306a9339ba835129884548b719f5c
elpapanuel <104025352+elpapanuel@users.noreply.github.com>
2024-10-23 21:41:20 -0500

    2

---
+++ TrabajoFinal 2.0/TrabajoFinal/src/test/java/edu/pe/idat/TrabajoFinal/TrabajoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.idat.TrabajoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
