f26d01a6aa768fd241dee990cc685269a54e33ef
JesusMcchaca <jesusmachaca23@gmail.com>
2024-10-27 21:05:30 -0500

    Controller
    Se implemento todos los controladores.

---
+++ TrabajoFinal/src/main/java/edu/pe/utp/TrabajoFinal/controller/TrabajadorController.java
@@ -0,0 +1,27 @@
+package edu.pe.utp.TrabajoFinal.controller;
+
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.SessionStatus;
+
+
+@Controller
+@RequestMapping("/oulala/trabajador/")
+public class TrabajadorController {
+
+	
+	
+
+
+	
+
+
+}
