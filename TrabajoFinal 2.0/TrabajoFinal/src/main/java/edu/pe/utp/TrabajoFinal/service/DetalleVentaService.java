5a842c46812b4f0df869e08b78afde638abe0ebb
elpapanuel <104025352+elpapanuel@users.noreply.github.com>
2024-10-27 20:34:28 -0500

    Delete TrabajoFinal 2.0/TrabajoFinal directory

--- TrabajoFinal 2.0/TrabajoFinal/src/main/java/edu/pe/utp/TrabajoFinal/service/DetalleVentaService.java
+++
@@ -1,9 +0,0 @@
-package edu.pe.utp.TrabajoFinal.service;
-
-
-import edu.pe.utp.TrabajoFinal.entity.DetalleVenta;
-
-
-public interface DetalleVentaService extends CrudService<DetalleVenta, Integer> {
-	
-}
--- TrabajoFinal 2.0/TrabajoFinal/src/main/java/edu/pe/utp/TrabajoFinal/service/ProveedorService.java
+++
@@ -1,9 +0,0 @@
-package edu.pe.utp.TrabajoFinal.service;
-
-
-import edu.pe.utp.TrabajoFinal.entity.Proveedor;
-
-
-public interface ProveedorService extends CrudService<Proveedor, Integer> {
-	
-}
